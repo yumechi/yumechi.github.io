41c04aa1dc0edf2fdc162c052558daafbca0354b
yumechi <yumechi0525@gmail.com>
2023-11-29 09:07:04 +0900

    add deploymentBranch

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -17,6 +17,7 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'yumechi', // Usually your GitHub org/user name.
   projectName: 'yumechi.github.io', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
